47a32f41b4697c7b0d9aba01454b739b6670a4a0
Sam Praneis <sampraneis@gmail.com>
2025-03-19 13:57:02 -0500

    add peek to queue; renames

--- simulator/sim/ds/queue.c
+++ simulator/sim/ds/queue.c
@@ -9,72 +9,80 @@ static void resize(queue *q);
 static int get_at(queue *q, int, int *);
 
 static void resize(queue *q) {
-  int *new_arr = malloc(sizeof(int) * (q->capacity * 2));
+  int *new_arr = malloc(sizeof(int) * (q->_capacity * 2));
 
-  for (size_t i = 0; i < q->size; i++) {
-    size_t index = (q->front + i) % q->capacity;
-    new_arr[i] = q->arr[index];
+  for (size_t i = 0; i < q->_size; i++) {
+    size_t index = (q->_front + i) % q->_capacity;
+    new_arr[i] = q->_arr[index];
   }
 
-  q->capacity = q->capacity * 2;
-  q->front = 0;
+  q->_capacity = q->_capacity * 2;
+  q->_front = 0;
 
-  free(q->arr);
-  q->arr = new_arr;
+  free(q->_arr);
+  q->_arr = new_arr;
 }
 
 void add(queue *q, int element) {
   if (q == NULL) {
     return;
   }
-  if (q->size == q->capacity) {
+  if (q->_size == q->_capacity) {
     resize(q);
   }
-  size_t index = (q->front + q->size) % q->capacity;
-  q->arr[index] = element;
-  q->size += 1;
+  size_t index = (q->_front + q->_size) % q->_capacity;
+  q->_arr[index] = element;
+  q->_size += 1;
 }
 int dequeue(queue *q, int *element) {
-  if (q == NULL || q->size == 0) {
+  if (q == NULL || q->_size == 0) {
     return -1;
   }
 
-  *element = q->arr[q->front];
-  q->front = (q->front + 1) % q->capacity;
-  q->size -= 1;
+  *element = q->_arr[q->_front];
+  q->_front = (q->_front + 1) % q->_capacity;
+  q->_size -= 1;
 
   return 0;
 }
 
 int get_at(queue *q, int index, int *element) {
-  if (q == NULL || index >= q->size) {
+  if (q == NULL || index >= q->_size) {
     return -1;
   }
-  size_t adj_index = (q->front + index) % q->capacity;
-  *element = q->arr[adj_index];
+  size_t adj_index = (q->_front + index) % q->_capacity;
+  *element = q->_arr[adj_index];
+  return 0;
+}
+
+int peek(queue *q, int *element) {
+  if (q == NULL || q->_size == 0) {
+    return -1;
+  }
+  *element = q->_arr[q->_front];
   return 0;
 }
 int size(queue *q) {
   if (q == NULL) {
     return -1;
   }
-  return q->size;
+  return q->_size;
 }
 void deallocate(queue **q) {
   if (q == NULL) {
     return;
   }
-  free((*q)->arr);
+  free((*q)->_arr);
   free(*q);
   *q = NULL;
 }
 
 queue *new_queue() {
   queue *q = malloc(sizeof(queue));
-  q->front = 0;
-  q->size = 0;
-  q->capacity = 10;
-  q->arr = malloc(q->capacity * sizeof(int));
+  q->_front = 0;
+  q->_size = 0;
+  q->_capacity = 10;
+  q->_arr = malloc(q->_capacity * sizeof(int));
 
   // func ptrs
   q->len = size;
@@ -82,6 +90,7 @@ queue *new_queue() {
   q->queue = add;
   q->dealloc = deallocate;
   q->get_at = get_at;
+  q->peek = peek;
 
   return q;
 }
--- simulator/sim/ds/queue.h
+++ simulator/sim/ds/queue.h
@@ -2,15 +2,16 @@
 #ifndef DS_QUEUE
 #define DS_QUEUE
 typedef struct queue {
-  int *arr;
-  size_t front;
-  size_t capacity;
-  size_t size;
+  int *_arr;
+  size_t _front;
+  size_t _capacity;
+  size_t _size;
   int (*len)(struct queue *);
   int (*dequeue)(struct queue *, int *element);
   void (*queue)(struct queue *, int element);
   void (*dealloc)(struct queue **);
   int (*get_at)(struct queue *, int index, int *element);
+  int (*peek)(struct queue *, int *element);
 } queue;
 
 queue *new_queue();
